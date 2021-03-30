4deb70a670f4290bd704b2c2a42e34edf6d0c8c6
BGUsha <ushabg6@gmail.com>
2021-03-30 15:12:55 +0530

    input output processing done

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 var btnTranslator = document.querySelector("#btn-translator");
 var txtInput = document.querySelector("#txt-input");
+var outputDiv = document.querySelector("#output");
+
 console.log(txtInput)
 function clickEventHandler(){
-    console.log("clicked");
-    console.log("input",txtInput.value);
+    outputDiv.innerText="hdghaad " + txtInput.value;
 }
 btnTranslator.addEventListener("click",clickEventHandler)
 
