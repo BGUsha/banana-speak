157f6af36190ac49f16dcc63f176f8a21a26eaaa
BGUsha <ushabg6@gmail.com>
2021-03-31 16:56:03 +0530

    background img and button design

--- app.js
+++ app.js
@@ -2,11 +2,30 @@ var btnTranslator = document.querySelector("#btn-translator");
 var txtInput = document.querySelector("#txt-input");
 var outputDiv = document.querySelector("#output");
 
-console.log(txtInput)
-function clickEventHandler(){
-    outputDiv.innerText="hdghaad " + txtInput.value;
+//var serverURL = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json";
+
+var serverURL = "https://api.funtranslations.com/translate/minion.json"
+
+function getTranslationURL(text){
+    return serverURL + "?" + "text=" + text
 }
+
+function errorHandler(error) {
+    console.log("error occured", error);
+    alert("something went wrong! try again after some time")
+}
+
+function clickEventHandler(){
+    var inputText = txtInput.value; //taking input
+
+    //calling server for processing
+    fetch(getTranslationURL(inputText))
+        .then(response => response.json())
+        .then(json => {
+            var translatedText = json.contents.translated;
+            outputDiv.innerText = translatedText; //output
+        })
+        .catch(errorHandler)
+};
+
 btnTranslator.addEventListener("click",clickEventHandler)
-
-    
-
